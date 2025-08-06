a0f6cb03801ee4d91210ba19761c31e37c8f0b81
kimsang21235 <144400584+kimsang21235@users.noreply.github.com>
2025-08-06 11:06:07 +0900

    Update 3정면.py

--- code/3정면.py
+++ code/3정면.py
@@ -12,3 +12,21 @@ df_filtered = df[df['info_filename'].str.contains('F', na=False)]
 df_filtered.to_csv("정면.csv", index=False, encoding="utf-8-sig")
 
 print("✅ 'F'가 포함된 행만 필터링 완료!")
+-----------------------------------------------------------------------
+# 사진 파일 정면 사진만 남기고 나머지 삭제
+import os
+
+base_dir = os.getcwd()  # 현재 작업 디렉터리
+
+for foldername, subfolders, filenames in os.walk(base_dir):
+    # 현재 탐색 중인 폴더명이 4자리 숫자인 경우만 처리
+    folder_basename = os.path.basename(foldername)
+    if folder_basename.isdigit() and len(folder_basename) == 4:
+        for filename in filenames:
+            if (("_L" in filename) or ("_R" in filename)) and filename.endswith(".jpg"):
+                file_path = os.path.join(foldername, filename)
+                try:
+                    os.remove(file_path)
+                    print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
